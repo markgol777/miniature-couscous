b0d9af7ee839bf51768a2ccceabf385082346a5d
markgol777 <markgol777@gmail.com>
2024-06-28 10:38:45 +0200

    d

--- src/components/home/section3/creator.js
+++ src/components/home/section3/creator.js
@@ -4,8 +4,8 @@ export default function Creator(props) {
   return (
     <div className="creator">
       <img src={props.img} alt=""/>
-      <p>{props.name}</p>
-      <p>Total sales: {props.totalSales}</p>
+      <h3>{props.name}</h3>
+      <p className="total-sales"><span className="total-sales-text">Total sales:</span> {props.totalSales}</p>
     </div>
   )
 }
--- src/components/home/section3/section3.js
+++ src/components/home/section3/section3.js
@@ -1,6 +1,18 @@
 import "./index.css";
 import rocket from "../../../assets/rocket.svg";
 import Creator from "./creator";
+import keepitreal from "../../../assets/keepitreal.svg";
+import digilab from '../../../assets/digilab.svg';
+import gravityone from '../../../assets/GravityOne.svg';
+import juanie from '../../../assets/Juanie.svg';
+import bluewhale from '../../../assets/BlueWhale.svg';
+import mr_fox from '../../../assets/mr_fox.svg';
+import Shroomie from '../../../assets/Shroomie.svg';
+import Robotica from '../../../assets/robotica.svg';
+import RustyRobot from '../../../assets/RustyRobot.svg';
+import Animakid from '../../../assets/animakid.svg';
+import Dotgu from '../../../assets/Dotgu.svg';
+import Ghiblier from '../../../assets/Ghiblier.svg'
 
 export default function Section3() {
   return (
@@ -18,10 +30,18 @@ export default function Section3() {
       </div>
 
       <div className="creators">
-        <Creator name="d" totalSales="asdf"/>
-        <Creator name="d" totalSales="asdf"/>
-        <Creator name="d" totalSales="asdf"/>
-        <Creator name="d" totalSales="asdf"/>
+        <Creator name="Keepitreal" img={keepitreal} totalSales="34.53 ETH"/>
+        <Creator name="DigiLab" img={digilab} totalSales="34.53 ETH"/>
+        <Creator name="GravityOne" img={gravityone} totalSales="34.53 ETH"/>
+        <Creator name="Juanie" img={juanie} totalSales="34.53 ETH"/>
+        <Creator name="BlueWhale" img={bluewhale} totalSales="34.53 ETH"/>
+        <Creator name="Mr Fox" img={mr_fox} totalSales="34.53 ETH"/>
+        <Creator name="Shroomie" img={Shroomie} totalSales="34.53 ETH"/>
+        <Creator name="Robotica" img={Robotica} totalSales="34.53 ETH"/>
+        <Creator name="RustyRobot" img={RustyRobot} totalSales="34.53 ETH"/>
+        <Creator name="Animakid" img={Animakid} totalSales="34.53 ETH"/>
+        <Creator name="Dotgu" img={Dotgu} totalSales="34.53 ETH"/>
+        <Creator name="Ghiblier" img={Ghiblier} totalSales="34.53 ETH"/>
 
       </div>
     </section>
